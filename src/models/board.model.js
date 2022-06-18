13ee121dec9b840aac8f33d926d6d2cd580c0ff4
TNuan <minibike090@gmail.com>
2022-06-18 19:08:18 +0700

    APIs get full board, columns and cards p1.

--- src/models/board.model.js
+++ src/models/board.model.js
@@ -1,5 +1,8 @@
 import Joi from 'joi'
+import { ObjectId } from 'mongodb'
 import { getDB } from '*/config/mongodb'
+import { ColumnModel } from './column.model'
+import { CardModel } from './card.model'
 
 //Degine Board Collection
 const boardCollectionName = 'boards'
@@ -25,5 +28,57 @@ const createNew = async (data) => {
   }
 }
 
-export const BoardModel = { createNew }
+/**
+ * 
+ * @param {string} boardId 
+ * @param {string} columnId 
+ */
+const pushColumnOrder = async (boardId, columnId) => {
+  try {
+    const result = await getDB().collection(boardCollectionName).findOneAndUpdate(
+      { _id: ObjectId(boardId) },
+      { $push: { columnOrder: columnId } },
+      { returnOriginal: false }
+    )
+
+    return result.value
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+const getFullBoard = async (boardId) => {
+  try {
+    const result = await getDB().collection(boardCollectionName).aggregate([
+      { $match: { _id: ObjectId(boardId) } },
+      // {
+      //   $addFields: {
+      //     _id: { $toString: '$_id'}
+      //   }
+      // },
+      { $lookup: {
+          from: ColumnModel.columnCollectionName, // collection name
+          localField: '_id',
+          foreignField: 'boardId',
+          as: 'columns'
+      } },
+      {  $lookup: {
+          from: CardModel.cardCollectionName, // collection name
+          localField: '_id',
+          foreignField: 'boardId',
+          as: 'cards'
+      } }
+    ]).toArray()
+
+    return result[0] || {}
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+export const BoardModel = { 
+  createNew,
+  pushColumnOrder,
+  getFullBoard
+}
 
--- src/models/card.model.js
+++ src/models/card.model.js
@@ -1,11 +1,12 @@
 import Joi from 'joi'
+import { ObjectId } from 'mongodb'
 import { getDB } from '*/config/mongodb'
 
 //Degine Card Collection
 const cardCollectionName = 'cards'
 const cardCollectionSchema = Joi.object({
-  boardId: Joi.string().required(),
-  columnId: Joi.string().required(),
+  boardId: Joi.string().required(),// Also ObjectId when create new 
+  columnId: Joi.string().required(),// Also ObjectId when create new 
   title: Joi.string().required().min(3).max(30).trim(),
   cover: Joi.string().default(null),
   createdAt: Joi.date().timestamp().default(Date.now()),
@@ -19,12 +20,20 @@ const validateSchema = async (data) => {
 
 const createNew = async (data) => {
   try {
-    const value = await validateSchema(data)
-    const result = await getDB().collection(cardCollectionName).insertOne(value)
+    const validatedValue = await validateSchema(data)
+    const insertValue = {
+      ...validatedValue,
+      boardId: ObjectId(validatedValue.boardId),
+      columnId: ObjectId(validatedValue.columnId)   
+    }
+    const result = await getDB().collection(cardCollectionName).insertOne(insertValue)
     return result
   } catch (err) {
     throw new Error(err)
   }
 }
 
-export const CardModel = { createNew }
+export const CardModel = { 
+  cardCollectionName,
+  createNew 
+}
